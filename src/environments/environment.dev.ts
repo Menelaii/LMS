bf2dc8ff18ca12f5c746a953a33cad63038a633e
d. zhezha <d.post2434@gmail.com>
2024-02-10 15:51:58 +0300

    Добавить подмену url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,11 +1,3 @@
-// export const environment = {
-//   ROOT_URL: 'http://localhost:8080/api/v1',
-//   SIGN_IN_URL: 'http://localhost:8080/api/v1/auth/sign-in',
-//   LESSONS_URL: 'http://localhost:8080/api/v1/lessons',
-//   TOKEN_KEY: 'jwt',
-//   EXP_DATE_KEY: 'expiresIn',
-// }
-
 export const environment = {
   ROOT_URL: 'http://localhost:4200/api/v1',
   SIGN_IN_URL: 'http://localhost:4200/api/v1/auth/sign-in',
